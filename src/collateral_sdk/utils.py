186da0bef1119a4e453563427249a924eb69153d
Paul Kwon <pkwon@taoshi.io>
2025-07-31 01:55:37 +0900

    Fix bugs and update examples

--- src/collateral_sdk/utils.py
+++ src/collateral_sdk/utils.py
@@ -1,6 +1,7 @@
 import bittensor
 import bittensor.utils
 from eth_typing import ChecksumAddress
+from scalecodec import ss58_decode, ss58_encode
 from web3 import Web3
 
 
@@ -24,6 +25,20 @@ def pubkey_to_h160(pubkey: str) -> ChecksumAddress:
     return Web3.to_checksum_address(address)
 
 
+def pubkey_to_ss58(pubkey: str) -> str:
+    """
+    Convert a public key to an SS58 address.
+
+    Args:
+        pubkey (str): The public key to convert.
+
+    Returns:
+        str: The corresponding SS58 address.
+    """
+
+    return ss58_encode(pubkey)
+
+
 def ss58_to_h160(address: str) -> ChecksumAddress:
     """
     Convert an SS58 address to an H160 address.
@@ -39,3 +54,17 @@ def ss58_to_h160(address: str) -> ChecksumAddress:
     pubkey = pubkey_bytes.hex()
 
     return pubkey_to_h160(pubkey)
+
+
+def ss58_to_pubkey(address: str) -> str:
+    """
+    Convert an SS58 address to a public key.
+
+    Args:
+        address (str): The SS58 address to convert.
+
+    Returns:
+        str: The corresponding public key.
+    """
+
+    return f"0x{ss58_decode(address)}"
